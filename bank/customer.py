a63965f3c8cef7919b96c96ea2a27b232ba88ef1
shaikahalru <sha101kah@gmail.com>
2025-09-21 22:04:54 +0300

    create Customer class

---
+++ bank/customer.py
@@ -0,0 +1,9 @@
+class Customer():
+    def __init__(self, id, first_name, last_name, password, checking, saving):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
+        self.checking = checking
+        self.saving = saving
+    
