8c32798fb3c58a9418f1e69e25fc65cb8bac31c1
shaikahalru <sha101kah@gmail.com>
2025-09-22 11:35:54 +0300

    test create customer object and account object

--- tests/test_account.py
+++ tests/test_account.py
@@ -4,15 +4,13 @@ from bank.account import Account
 class TestAccount(unittest.TestCase):
 
     def setUp(self):
-        self.account_s = Account(2382, 1000, 2000, 'Saving')
-        self.account_c = Account(9651, 1000, 1000, 'Checking')
+        self.account_s = Account(1000, 2000, 'Saving')
+        self.account_c = Account(1000, 1000, 'Checking')
 
     def test_create_account_object(self):
-        self.assertEqual(self.account_s.id, 2382)
         self.assertEqual(self.account_s.owner_id, 1000)
         self.assertEqual(self.account_s.balance, 2000)
         self.assertEqual(self.account_s.type, 'Saving')
-        self.assertEqual(self.account_c.id, 9651)
         self.assertEqual(self.account_c.owner_id, 1000)
         self.assertEqual(self.account_c.balance, 1000)
         self.assertEqual(self.account_c.type, 'Checking')
--- tests/test_customer.py
+++ tests/test_customer.py
@@ -4,13 +4,10 @@ from bank.customer import Customer
 class TestCustomer(unittest.TestCase):
 
     def setUp(self):
-        self.customer = Customer(1000, 'Shaikah', 'Alrubayan', '16@Bs88hJ', 1000, 2000)
+        self.customer = Customer(1000, 'Shaikah', 'Alrubayan', '16@Bs88hJ')
     
     def test_create_customer_object(self):
         self.assertEqual(self.customer.id, 1000)
         self.assertEqual(self.customer.first_name, 'Shaikah')
         self.assertEqual(self.customer.last_name, 'Alrubayan')
         self.assertEqual(self.customer.password, '16@Bs88hJ')
-        self.assertEqual(self.customer.checking, 1000)
-        self.assertEqual(self.customer.saving, 2000)
-
