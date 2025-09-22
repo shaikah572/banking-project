308145bd4188eecfc958e96e9e0f73767d33eb88
shaikahalru <sha101kah@gmail.com>
2025-09-22 09:38:12 +0300

    monday standup

--- bank/bank.py
+++ bank/bank.py
@@ -1,7 +1,10 @@
 import random
+import csv
 
 class Bank:
     def __init__(self):
         customers = {}
         accounts = {}
         id_generator = random.randint(1000, 9999)
+    
+    # bank methods here 
