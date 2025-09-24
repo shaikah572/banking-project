64c5c78c09ccfd1f7fd0209d349c2c95d6e009d0
shaikahalru <sha101kah@gmail.com>
2025-09-24 13:57:04 +0300

    create deposit method in account class

--- bank/account.py
+++ bank/account.py
@@ -1,3 +1,5 @@
+from bank.custom_exceptions import *
+
 class Account:
     def __init__(self, owner_id, balance, type ):
         self.owner_id = owner_id
@@ -7,6 +9,10 @@ class Account:
         self.is_active = True
     
     # deposit, withdraw, transer methods
+    def deposit(self, amount):
+        if not self.is_active:
+            raise AccountInactiveError('Account is not active.')
+        self.balance += amount
 
     def __str__(self):
         return f'\nCustomer ID: {self.owner_id} \nAccount type: {self.type} \nBalance: {self.balance}'
